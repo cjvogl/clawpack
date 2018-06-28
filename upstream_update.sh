d82b1b4495b0abb353d10bd0e2a61320fa27af15
Chris Vogl <vogl2@llnl.gov>
2018-06-27 17:34:13 -0700

    modified upstream update script

--- upstream_update.sh
+++ upstream_update.sh
@@ -1,37 +1,41 @@
 #!/bin/bash
 
-cd $CLAW/apps
-git checkout master
-git pull upstream master
-git push -u origin master
+echo 'Updating AMRClaw'
 cd $CLAW/amrclaw
 git checkout master
 git pull upstream master
 git push -u origin master
+echo 'Updating Classic'
 cd $CLAW/classic
 git checkout master
 git pull upstream master
 git push -u origin master
+echo 'Updating Clawpack'
 cd $CLAW/clawpack
 git checkout master
 git pull upstream master
 git push -u origin master
+echo 'Updating ClawUtil'
 cd $CLAW/clawutil
 git checkout master
 git pull upstream master
 git push -u origin master
+echo 'Updating GeoClaw'
 cd $CLAW/geoclaw
 git checkout master
 git pull upstream master
 git push -u origin master
+echo 'Updating PyClaw'
 cd $CLAW/pyclaw
 git checkout master
 git pull upstream master
 git push -u origin master
+echo 'Updating Riemann'
 cd $CLAW/riemann
 git checkout master
 git pull upstream master
 git push -u origin master
+echo 'Updating VisClaw'
 cd $CLAW/visclaw
 git checkout master
 git pull upstream master
